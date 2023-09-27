ccaec5d209456f656058b51681fd3ada8ded79bc
Najam <85332859+najmiter@users.noreply.github.com>
2023-09-27 10:53:05 +0500

    Update std.asm

--- src/std.asm
+++ src/std.asm
@@ -17,6 +17,9 @@ section .text
 ;;;;;;;;;;;;;;;;;;;;;;;;; MACROS ;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;;;; print a const string ;;;;;;;;;;;;;;;;;;
+
 %macro println 1
 section .data
     string db %1, 0
@@ -24,7 +27,48 @@ section .data
 section .text
     mov rsi, string
     call std__cout    
+%endmacro
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;; push all registers on stack ;;;;;;;;;;;;;;
 
+%macro pushaq 0
+    push rax
+    push rbx
+    push rcx
+    push rdx
+    push rsi
+    push rdi
+    push rbp
+    push rsp
+    push r8
+    push r9
+    push r10
+    push r11
+    push r12
+    push r13
+    push r14
+    push r15
+%endmacro
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;; pop all registers on stack ;;;;;;;;;;;;;;
+
+%macro popaq 0
+    pop r15
+    pop r14
+    pop r13
+    pop r12
+    pop r11
+    pop r10
+    pop r9
+    pop r8
+    pop rsp
+    pop rbp
+    pop rdi
+    pop rsi
+    pop rdx
+    pop rcx
+    pop rbx
+    pop rax
 %endmacro
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -157,7 +201,7 @@ section .text
     mov rdx, 1
     syscall
 %endmacro
-
+    pushaq
     push rax      
 
     mov rcx, 1            
@@ -199,7 +243,8 @@ section .text
 
     mov byte [_char], 10
     writechr _char
-    
+
+    popaq
     ret
 
 
